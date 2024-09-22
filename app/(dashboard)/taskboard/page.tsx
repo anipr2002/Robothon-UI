1b83d02cdb9a58b0f5badd62a17344fbe866d69d
Anirudh <anirudh.panchangamranganath@thws.study.de>
2024-09-22 20:26:01 +0200

    :tada: Initial Commit

---
+++ app/(dashboard)/taskboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskFlow from "../_components/TaskFlow";
+
+const page = () => {
+  return (
+    <>
+      <TaskFlow />
+    </>
+  );
+};
+
+export default page;
